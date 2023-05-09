3fbc908432d478630e965e718e4759a9f176644e
Thomas <taadamson33@gmail.com>
2023-05-08 21:56:13 -0400

    added initial client / server file structure

---
+++ client/src/components/createPost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//form to create a new post
+const createPost = () => {
+  return (
+    <div>createPost</div>
+  )
+}
+
+export default createPost
---
+++ client/src/components/post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//rendered for full page view --> post details + comments
+
+const post = () => {
+  return (
+    <div>post</div>
+  )
+//
+
+}
+
+export default post
