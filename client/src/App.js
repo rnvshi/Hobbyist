20b4d0e234174ce2f6ae318c7969d5ce69d382d5
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-05-10 15:39:42 -0700

    App, Routes, COnidtional Logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/home';
+import Profile from './pages/profile';
+// import { useQuery } from "apollo/client"
 function App() {
+  // const { data, loading, error } = useQuery(QueryMe)
+  // const user = data?.me
+  const user = false // Delete once replaced with real user
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <navBar />
+
+
+        {user ?
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+
+
+            <Route path="/profile" element={<Profile />} />
+
+
+          </Routes> :
+
+          <Routes>
+
+            <Route path="/" element={<h1>signup</h1>} />
+
+          </Routes>}
+
+      </Router>
+
+
+
     </div>
   );
 }
