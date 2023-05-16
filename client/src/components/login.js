a95638b35d192d2d71e2ddbc9721177202cb4ac6
Thomas <taadamson33@gmail.com>
2023-05-15 20:11:39 -0400

    allows users to login / logout from application

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,17 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
 //form to login
 const Login = () => {
+  const [formState, setFormState] = useState({ userName: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
   return (
 
     <div id="signup-form-flex">
-      <form id="signup-form">
-        <div class="signup-fields">
-          <label class="signup-form-label">User Name</label>
-          <input id="userName" />
+      <form 
+      id="signup-form"
+      onSubmit={handleFormSubmit}
+      >
+        <div className="signup-fields">
+          <label className="signup-form-label">User Name</label>
+          <input 
+          id="userName"
+          name='userName'
+          value={formState.userName}
+          onChange={handleChange} 
+          />
         </div>
-        <div class="signup-fields">
-          <label class="signup-form-label" id="password-login-form">Password</label>
-          <input id="password-login-form" />
+        <div className="signup-fields">
+          <label className="signup-form-label" id="password-login-form">Password</label>
+          <input 
+          id="password-login-form"
+          name='password'
+          type="password"
+          value={formState.password}
+          onChange={handleChange} 
+           />
         </div>
         <button id="signup-button" type="submit">LOG IN</button>
       </form>
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,26 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profile from "../pages/profile"
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 //form to signup
 const Signup = () => {
+
+  const [formState, setFormState] = useState({ firstName: '', lastName: '', userName: '', email: '', password: '' });
+  const [signup, { error, data }] = useMutation(CREATE_USER);
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await signup({
+        variables: { ...formState },
+      });
+      console.log(data)
+      Auth.login(data.createUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div id="signup-form-flex">
-      <form id="signup-form">
-
-        <div class="signup-fields">
-          <label class="signup-form-label">User Name</label>
-          <input id="userName" />
+      <form 
+      id="signup-form"
+      onSubmit={handleFormSubmit}
+      >
+      <div className="signup-fields">
+          <label className="signup-form-label">First Name</label>
+          <input 
+          id="FirstName" 
+          name='firstName'
+          value={formState.firstName}
+          onChange={handleChange} 
+          />
         </div>
 
-        <div class="signup-fields">
-          <label id="email-label" class="signup-form-label">E-mail</label>
-          <input id="email"></input>
+        <div className="signup-fields">
+          <label className="signup-form-label">Last Name</label>
+          <input 
+          id="lastName"
+          name='lastName'
+          value={formState.lastName}
+          onChange={handleChange} 
+          />
         </div>
-        <div class="signup-fields">
-          <label id="password-label" class="signup-form-label">Password</label>
-          <input id="password" />
+
+        <div className="signup-fields">
+          <label className="signup-form-label">User Name</label>
+          <input 
+          id="userName"
+          name='userName'
+          value={formState.userName}
+          onChange={handleChange} 
+          />
         </div>
-        <a id="signinButtonLink" href={Profile}><button id="signup-button" type="login">SIGN UP</button></a>
+
+        <div className="signup-fields">
+          <label id="email-label" className="signup-form-label">E-mail</label>
+          <input 
+          id="email"
+          name='email'
+          value={formState.email}
+          onChange={handleChange} 
+          ></input>
+        </div>
+        <div className="signup-fields">
+          <label id="password-label" className="signup-form-label">Password</label>
+          <input 
+          id="password"
+          name='password'
+          type="password"
+          value={formState.password}
+          onChange={handleChange}  
+          />
+        </div>
+
+        <button id="signup-button" type="submit">SIGN UP</button>
       </form>
     </div>
 
