e2935672c77cffeb4079aa43ca2175af047aff17
Isha Rahman <ishaarahman@gmail.com>
2023-05-15 20:47:29 -0400

    search user by username and console details

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navigation from '../components/navBar'
 import hobbylogo from '../images/hobbylogo.png'
 import Footer from '../components/footer'
 import { Link } from "react-router-dom";
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_USERNAME } from '../utils/queries';
 
-const search = () => {
+const Search = () => {
+
+  const [searchState, setSearchState] = useState({
+    username: ''
+  });
+
+  const [searchUsername, { loading, data, error }] = useLazyQuery(QUERY_USERNAME);
+
+  const handleChange = (event) => {
+
+    const { value } = event.target;
+
+    setSearchState({
+      username: value
+    });
+
+  };
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    console.log(searchState);
+
+    const { data } = await searchUsername({
+      variables: { ...searchState },
+    });
+
+    console.log(data.singleUsername.userName);
+    console.log(data.singleUsername.bio);
+    console.log(data.singleUsername.avatar);
+  };
+
   return (
+
     <div>
 
-      <div class="search-container">
-        <input type="text" placeholder="Search Friend..." class="search-input" />
-        <button type="submit" class="search-button">Search</button>
-      </div>
-
-
-
-
+      <form class="search-container" onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search Friend..."
+          class="search-input"
+          value={searchState.username}
+          onChange={handleChange} />
+        <button
+          type="submit"
+          class="search-button">
+          Search</button>
+      </form>
 
     </div>
-  )
-}
+  );
+};
 
-export default search
+
+
+export default Search;
